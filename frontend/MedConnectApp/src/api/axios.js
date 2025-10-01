1e4f71fe34be83623d4421637f64e2eabfa37c9b
ajaybibin12 <ajaybibin12@gmail.com>
2025-10-01 16:07:00 +0530

    Added backend API url.

--- frontend/MedConnectApp/src/api/axios.js
+++ frontend/MedConnectApp/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", // FastAPI backend
+  // baseURL: "http://127.0.0.1:8000", // FastAPI backend
+  baseURL: "https://medconnect-api-xly3.onrender.com",
 });
 
 export default api;
